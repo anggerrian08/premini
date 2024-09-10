a6022909195eb81ea5c4e03875b842bcb767e367
anggerrian08 <anggeraderiandipa@gmail.com>
2024-09-10 09:43:01 +0700

    css
    add css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,122 +20,96 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('resources/css/style.css') }}">
+    @vite('resources/css/style.css')
+
 </head>
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-black bg-secondary shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="#" style="font-size: 1.5rem; font-weight: bold; color: #173B45;">
-                    <style="height: 40px;">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm mb-0">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#" style="font-size: 1.5rem; font-weight: bold;">
                     CoffeeShop
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                @auth
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav me-auto">
-                            <!-- Menu items here -->
-                        </ul>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto"></ul>
 
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ms-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                                @if (Route::has('login'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                    </li>
-                                @endif
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
 
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>
-                                @endif
-                            @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }}
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
                                     </a>
 
-                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                            @endguest
-                        </ul>
-                    </div>
-                @endauth
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
         </nav>
 
         <div class="container-fluid">
-            <style>
-                .sidebar {
-                    background-color: #454647;
-                    padding: 20px;
-                    border-right: 1px solid #ddd;
-                }
-
-                .sidebar .nav-link {
-                    color: #8a6e11;
-                    font-weight: bold;
-                }
-
-                .sidebar .nav-link:hover {
-                    color: #697029;
-                }
-            </style>
             <div class="row">
                 <!-- Sidebar -->
-                <aside class="col-md-3 sidebar">
-
+                <aside class="col-md-3 bg-secondary p-3 min-vh-100 border-end">
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/deskripsi') }}">{{ __('Tentang Kami') }}</a>
+                            <a class="nav-link text-light" href="{{ url('/deskripsi') }}">{{ __('Tentang Kami') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/customer') }}">{{ __('Pelanggan') }}</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/kategori') }}">{{ __('Kategori produk') }}</a>
+                            <a class="nav-link text-light" href="{{ url('/customer') }}">{{ __('Pelanggan') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/supplayer') }}">{{ __('Supplayer') }}</a>
+                            <a class="nav-link text-light" href="{{ url('/kategori') }}">{{ __('Kategori Produk') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/produk') }}">{{ __('Menu') }}</a>
+                            <a class="nav-link text-light" href="{{ url('/supplayer') }}">{{ __('Supplayer') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/stok') }}">{{ __('Stok') }}</a>
+                            <a class="nav-link text-light" href="{{ url('/produk') }}">{{ __('Menu') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/order') }}">{{ __('Order') }}</a>
+                            <a class="nav-link text-light" href="{{ url('/stok') }}">{{ __('Stok') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/karyawan') }}">{{ __('Karyawan') }}</a>
+                            <a class="nav-link text-light" href="{{ url('/order') }}">{{ __('Order') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="{{ url('/karyawan') }}">{{ __('Karyawan') }}</a>
                         </li>
                     </ul>
-
-                    <!-- Optional: Move the table to the sidebar if needed -->
-
                 </aside>
 
                 <!-- Main Content -->
